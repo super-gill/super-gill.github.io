cf0c610b93ee4e2d7706233600cf7e04008dc478
Jason <jason@mcdill.uk>
2023-02-11 15:13:01 +0000

    interactive rating component

--- frontend-mentor/interactive-rating-component-main/java.js
+++ frontend-mentor/interactive-rating-component-main/java.js
@@ -4,7 +4,7 @@ for (i = 0; i < 5; i++) {
         var thisButton = parseInt(this.textContent);
         buttonIDIndex = [];
         var buttonID = ["one", "two", "three", "four", "five"];
-
+        var submitReady = "false";
 
         switch (thisButton) {
             case 1: modifyButton(); break;
@@ -17,10 +17,12 @@ for (i = 0; i < 5; i++) {
 
         function modifyButton() {
             document.getElementById(buttonID[thisButton - 1]).classList.add("onClick");
+            document.querySelector(".submit-btn").classList.add("superSubmit");
             buttonIDIndex = buttonID.indexOf(buttonID[thisButton - 1]);
             buttonID.splice(buttonIDIndex, 1);
             resetOnClick(buttonID);
-            console.log(thisButton);
+            submitReady = "true";
+            submit(submitReady);
             document.getElementById("scoreNumber").innerHTML = thisButton;
         }
 
@@ -28,6 +30,15 @@ for (i = 0; i < 5; i++) {
             for (i = 0; i < 4; i++) {
                 document.getElementById(buttonID[i]).classList.remove("onClick");
             }
-        }
+        }     
+    })
+}
+
+function submit(submitReady) {
+    document.querySelector(".submit-btn").addEventListener("click", function () {
+        if (submitReady == "true") {
+            document.getElementById("card-front").classList.add("hide");
+            document.getElementById("card-back").classList.remove("hide");
+        } else { console.log("submit if failed") };
     })
 }
