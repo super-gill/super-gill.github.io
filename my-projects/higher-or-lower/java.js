1ed0eb9f87e258e50fbd7fc502a1900d80a14515
Jason <121322560+super-gill@users.noreply.github.com>
2023-03-15 16:36:53 +0000

    higher or lower

--- my-projects/higher-or-lower/java.js
+++ my-projects/higher-or-lower/java.js
@@ -1,7 +1,3 @@
-var suitClubs = [];
-var suitDiamonds = [];
-var suitHearts = [];
-var suitSpade = [];
 var deck = [];
 var playHistoryArray = [];
 var userGuess = undefined;
@@ -16,6 +12,12 @@ var activeCardRandomizer;
 var nextCardRandomizer;
 var activeCardValue;
 var nextCardValue = 'default';
+var autoPilotEnable = "false";
+var gameCycle = 0;
+var initialDeckLength;
+var deckLengthCheck;
+
+var level
 
 const activeCardIMGLoc = document.querySelector("#activeCardIMG");
 const nextCardIMGLoc = document.querySelector("#nextCardIMG");
@@ -26,6 +28,9 @@ const scoreLoc = document.querySelector("#score");
 const cheatButtonLoc = document.querySelector("#cheat");
 const deckCountLoc = document.querySelector("#deck-count");
 const imgContainerLoc = document.getElementById('img-container');
+const allButtonsLoc = document.querySelectorAll("button");
+const higherButtonLoc = document.querySelector("#high")
+const lowerButtonLoc = document.querySelector("#low")
 
 titleLoc.innerHTML = "Higher or Lower!"
 cheatButtonLoc.innerHTML = "Cheat! (" + cheatLives + ") lives left"
@@ -35,14 +40,64 @@ nextCardIMGLoc.setAttribute("src", "Assets/PNG-cards-1.3/red_joker.png");
 
 buildDeck();
 
+function autoPilot() {
+    // for testing, play a perfect game without any "guessing"
+    // console.log("autoPilot() ran");
+    autoPilotEnable = true;
+    allButtonsLoc.forEach((button) => {
+        button.disabled = true;
+    })
+    const intervalId = setInterval(() => {
+        if (nextCardValue >= activeCardValue) {
+            clearInterval(intervalId);
+            higher();
+        } else if (activeCardValue >= nextCardValue) {
+            clearInterval(intervalId);
+            lower();
+        }
+    }, 1000);
+}
+
+function daniel(level) {
+    var dansGuess;
+    var debugDecision = [];
+
+    // if the card is an Ace or a king, always choose higher or lower correspondingly
+    if (activeCardValue == 1) {
+        makeAIGuess(1);
+        debugDecision.push("stage 1: higher");
+    } else if (activeCardValue == 13) {
+        makeAIGuess(0);
+        debugDecision.push("stage 1: lower");
+    }
+
+
+    //if the previous card was an ace and the current card is not a king choose higher
+
+
+    function makeAIGuess(dansGuess) {
+        switch (dansGuess) {
+            case 1:
+                higher();
+                break;
+            case 0:
+                lower();
+                break;
+        } return;
+    } return;
+
+}
+
 function gameStart() {
+    // console.log("gameStart() ran");
     resetGame();
     titleLoc.innerHTML = "Building the deck!";
     scoreBoard("closescoreboard");
-    setTimeout(() => buildDeck(), 2000);
+    setTimeout(() => buildDeck(), 1000);
 }
 
 function checkDeckCounterAmount() {
+    // console.log("checkDeckCounterAmount() ran");
     if (deck.length == 1) {
         deckCountLoc.innerHTML = "Last card!"
     } else if (deck.length == 0) {
@@ -53,6 +108,7 @@ function checkDeckCounterAmount() {
 }
 
 function updatePoints() {
+    // console.log("updatePoints() ran");
     if (score != 0 && score - 1 % 5 == 0) {
         scoreLoc.innerHTML = "Get another one right and ill give you another cheat life!"
     }
@@ -82,6 +138,7 @@ function updatePoints() {
 }
 
 function updateHistory(selection) {
+    // console.log("checkDeckCounterAmount() ran with the input: " + selection);
     switch (selection) {
         case "add":
             const img = document.createElement('img');
@@ -98,40 +155,41 @@ function updateHistory(selection) {
 }
 
 function buildDeck() {
-    checkDeckCounterAmount();
-    if (score === '0' ?? deck.length == 0) {
-        gameStart();
-    } else {
-        let i;
-        for (i = 1; i <= 13; i++) {
-            suitDiamonds.push(i + "_of_diamonds.png");
+    const suits = {
+        diamonds: "Diamonds",
+        hearts: "Hearts",
+        clubs: "Clubs",
+        spades: "Spades"
+    };
+
+    const cards = [];
+    for (let i = 1; i <= 13; i++) {
+        for (const suit in suits) {
+            const card = i + "_of_" + suits[suit] + ".png";
+            cards.push(card);
         }
-        for (i = 1; i <= 13; i++) {
-            suitHearts.push(i + "_of_hearts.png");
-        }
-        for (i = 1; i <= 13; i++) {
-            suitClubs.push(i + "_of_clubs.png");
-        }
-        for (i = 1; i <= 13; i++) {
-            suitSpade.push(i + "_of_spades.png");
-        }
-        deck = suitClubs.concat(suitDiamonds, suitHearts, suitSpade);
-        checkDeckCounterAmount()
-        cardChoice();
     }
-};
+    deck = cards;
+    checkDeckCounterAmount(); // make sure this function is defined
+    cardChoice(); // make sure this function is defined
+    if (score === '0') {
+        gameStart();
+    } else if (deck.length === 0 && score !== 0) {
+        gameOver();
+    }
+}
 
 function cardChoice() {
+    // console.log("cardChoice() ran");
     titleLoc.innerHTML = "Make your guess: Higher or Lower!"
     userGuess = undefined;
     updatePoints()
     activeSequenceCard();
 
     function activeSequenceCard() {
-
         if (nextCardValue == 'default') {
-            activeCardRandomizer = Math.floor((Math.random() * deck.length));
-            activeCard = deck[activeCardRandomizer];
+            activeCard = deck.splice(Math.floor(Math.random() * deck.length), 1)[0];
+            console.log("activeCard is: " + activeCard + " and there are " + deck.length + " cards left in the deck")
         } else {
             activeCard = nextCard;
         }
@@ -141,52 +199,51 @@ function cardChoice() {
     }
 
     function nextSequenceCard() {
+        // console.log("nextSequenceCard() ran");
         nextCardRandomizer = Math.floor((Math.random() * deck.length));
         nextCard = deck[nextCardRandomizer];
         createCardObject()
     }
-    console.log("Next card is: " + nextCard)
 }
 
 function createCardObject() {
+    // console.log("createCardObject() ran");
     activeCardValue = activeCard.split(" ");
     activeCardValue = parseInt(activeCardValue.splice(0, 1));
     nextCardValue = nextCard.split(" ");
     nextCardValue = parseInt(nextCardValue.splice(0, 1));
     buttonDisable = false;
     cheat.disabled = false;
+    if (autoPilotEnable == true) {
+        //setTimeout(() => autoPilot(), 1000)
+        autoPilot()
+    } return;
 }
 
 function higher() {
-    cheat.disabled = true;
-    if (buttonDisable == true) {
-    } else {
-        userGuess = 1;
-        buttonDisable = true;
-        checkUserGuess()
-        checkDeckCounterAmount();
-    }
+    userGuess = 1;
+    buttonDisable = true;
+    checkUserGuess()
+    checkDeckCounterAmount();
 }
 
+
 function lower() {
-    cheat.disabled = true;
-    if (buttonDisable == true) {
-    } else {
-        userGuess = 2;
-        buttonDisable = true;
-        checkUserGuess()
-        checkDeckCounterAmount();
-    }
+    userGuess = 2;
+    buttonDisable = true;
+    checkUserGuess()
+    checkDeckCounterAmount();
 }
 
 function cheatTurn() {
+    // console.log("cheatTurn() ran");
     if (cheatLives > 0) {
-        cheat.disabled = true;        
+        cheat.disabled = true;
         if (activeCardValue >= nextCardValue) {
             cheat.innerHTML = "LOWER!"
         } else {
             cheat.innerHTML = "HIGHER!"
-        }        
+        }
         cheatLives--;
     } else {
         cheat.innerHTML = "ALL OUTTA LIVES!";
@@ -197,6 +254,7 @@ function cheatTurn() {
 
 
 function checkUserGuess() {
+    // console.log("checkUserGuess() ran");
     if (userGuess == false) {
     } else {
         switch (userGuess) {
@@ -222,33 +280,57 @@ function checkUserGuess() {
 }
 
 function updateLives() {
+    // console.log("updateLives() ran");
     if (score % 3 == 0) {
         cheatLives++;
     }
 }
 
 function guessCorrect() {
+    // console.log("guessCorrect() ran");
+
+    //initialize the correct guess game state
     titleLoc.innerHTML = "CORRECT!"
-    userGuess = undefined;
     nextCardIMGLoc.setAttribute("src", "Assets/PNG-cards-1.3/" + activeCard);
     gameHistory.push(" " + activeCard);
+    initialDeckLength = deck.length;
+
+    //check if this is the last card and if it is build a new deck and stop executing guessCorrect
+    if (deck.length == 1) {
+        setTimeout(() => gameOver(), 1000);
+        return;
+    }
+
+    //update the correct guess game state
     deck.splice(activeCardRandomizer, 1);
     updateHistory("add");
     score++;
     updateLives();
     cheatButtonLoc.innerHTML = "Cheat! (" + cheatLives + ") lives left"
     updatePoints();
-    activeCardIMGLoc.setAttribute("src", "");
-    setTimeout(() => cardChoice(), 2000);
+    activeCardIMGLoc.setAttribute("src", "Assets/PNG-cards-1.3/" + nextCard);
+    console.log("Next card is: " + nextCard);
+
+    //update the game cycle
+    gameCycle++
+    console.log("Game cycle: " + gameCycle + " <-----------------")
+
+    //end the correct guess game state and continue the game
+    setTimeout(() => cardChoice(), 1000);
 }
 
 function gameOver() {
+    // console.log("gameOver() ran");
+    allButtonsLoc.forEach((button) => {
+        button.disabled = true;
+    })
     titleLoc.innerHTML = (" GAME OVER !");
     activeCardIMGLoc.setAttribute("src", "Assets/PNG-cards-1.3/" + nextCard);
     setTimeout(() => scoreBoard("openscoreboard"), 3000)
 }
 
 function scoreBoard(input) {
+    // console.log("scoreBoard() ran with the input: " + input);
     switch (input) {
         case "openscoreboard":
             document.querySelector("#play-area").classList.add("hidden");
@@ -262,6 +344,11 @@ function scoreBoard(input) {
 }
 
 function resetGame() {
+    // console.log("resetGame() ran");
+    allButtonsLoc.forEach((button) => {
+        button.disabled = false;
+    })
+
     document.querySelector("#play-area").classList.remove("hidden");
     document.querySelector("#game-over").classList.add("hidden");
     activeCardIMGLoc.setAttribute("src", "Assets/PNG-cards-1.3/red_joker.png");
