ce6c87b73ba7445289dc85db98c0698ee8143a0c
Jason <121322560+super-gill@users.noreply.github.com>
2024-02-16 22:10:51 +0000

    asd

--- Work/PowerShell/Scripts/AAD Broker.ps1
+++ Work/PowerShell/Scripts/AAD Broker.ps1
@@ -1,2 +1,9 @@
 $rand = get-random
-write-output "Random number: " $rand
+$userName = $Env:UserName
+$newName = "Microsoft.AAD.BrokerPlugin_cw5n1h2txyewy." + $rand
+$filePath = "c:\users\" + $userName + "\appdata\local\packages\Microsoft.AAD.BrokerPlugin_cw5n1h2txyewy"
+write-output $newName`n$filePath
+
+# Rename-Item -path $filePath -NewName $newName
+
+
